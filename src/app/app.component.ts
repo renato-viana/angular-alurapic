b2e4e5f5fce2c16d7ce540880184afc67be82b40
Renato Viana <renatoviana30@gmail.com>
2020-05-10 13:10:16 -0300

    Criando componente, módulo. Diretiva ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'alurapic';
-  description = 'Lobo Negro';
-  url = 'https://i.pinimg.com/originals/29/8e/84/298e84a971fd682be9b6f000b98385b0.jpg';
+
+  photos = [
+    {
+      url: 'https://i.pinimg.com/originals/29/8e/84/298e84a971fd682be9b6f000b98385b0.jpg',
+      description: 'Lobo Negro'
+    },
+    {
+      url: 'https://i.pinimg.com/originals/b3/eb/57/b3eb5772fe75779130432bacf513527c.jpg',
+      description: 'Lobo Branco'
+    }
+    {
+      url: 'https://i.pinimg.com/originals/33/2f/67/332f67355a88f803142aa7428db00feb.jpg',
+      description: 'Lobo Prata'
+    }
+  ];
+ 
 }
