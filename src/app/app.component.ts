636f961a505632b682d563e7307d0dd370ab0091
Renato Viana <renatoviana30@gmail.com>
2020-05-10 16:32:03 -0300

    Integração com Web API's

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from './photos/photo/photoService';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  photos = [
-    {
-      url: 'https://i.pinimg.com/originals/29/8e/84/298e84a971fd682be9b6f000b98385b0.jpg',
-      description: 'Lobo Negro'
-    },
-    {
-      url: 'https://i.pinimg.com/originals/b3/eb/57/b3eb5772fe75779130432bacf513527c.jpg',
-      description: 'Lobo Branco'
-    }
-    {
-      url: 'https://i.pinimg.com/originals/33/2f/67/332f67355a88f803142aa7428db00feb.jpg',
-      description: 'Lobo Prata'
-    }
-  ];
+  photos: any[] = [];
+  
+  constructor(private photoService: PhotoService) {} 
+  
+  ngOnInit(): void {
+    
+    this.photoService
+      .listFromUser('flavio')
+      .subscribe(photos => this.photos = photos);
+  }
  
 }
