16c9f8c9c207c661eb2efc49207482306fc6eac2
Renato Viana <renatoviana30@gmail.com>
2020-05-10 11:28:46 -0300

    Adequando o template gerado e mais data binding

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'alurapic';
+  description = 'Lobo Negro';
+  url = 'https://i.pinimg.com/originals/29/8e/84/298e84a971fd682be9b6f000b98385b0.jpg';
+}
